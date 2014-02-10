fb0b1415e8160b531275da9a0c4eedf3810c4de0
xiaopan <sky@xiaopan.me>
2014-02-10 16:34:29 +0800

    231

--- sample/me/xiaopan/android/viewplayer/sample/MainActivity.java
+++ sample/me/xiaopan/android/viewplayer/sample/MainActivity.java
@@ -7,9 +7,12 @@ import me.xiaopan.android.viewplayer.PagerPlayer;
 import me.xiaopan.android.viewplayer.PlayMode;
 import me.xiaopan.android.viewplayer.R;
 import me.xiaopan.easy.imageloader.ImageLoader;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v4.view.ViewPager.PageTransformer;
+import android.view.View;
 
 public class MainActivity extends FragmentActivity {
 	private PagerPlayer pagerPlayerCircle;
@@ -70,5 +73,37 @@ public class MainActivity extends FragmentActivity {
 		pagerPlayerCircle.stop();
 		pagerPlayerSwing.stop();
 		super.onPause();
+	}
+	
+	public class DepthPageTransformer implements PageTransformer {
+	    private static final float MIN_SCALE = 0.75f;
+	    @SuppressLint("NewApi")
+	    @Override
+	    public void transformPage(View view, float position) {
+	        int pageWidth = view.getWidth();
+	        if (position < -1) { // [-Infinity,-1)
+	                                // This page is way off-screen to the left.
+	            view.setAlpha(0);
+	        } else if (position <= 0) { // [-1,0]
+	                                    // Use the default slide transition when
+	                                    // moving to the left page
+	            view.setAlpha(1);
+	            view.setTranslationX(0);
+	            view.setScaleX(1);
+	            view.setScaleY(1);
+	        } else if (position <= 1) { // (0,1]
+	                                    // Fade the page out.
+	            view.setAlpha(1 - position);
+	            // Counteract the default slide transition
+	            view.setTranslationX(pageWidth * -position);
+	            // Scale the page down (between MIN_SCALE and 1)
+	            float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
+	            view.setScaleX(scaleFactor);
+	            view.setScaleY(scaleFactor);
+	        } else { // (1,+Infinity]
+	                    // This page is way off-screen to the right.
+	            view.setAlpha(0);
+	        }
+	    }
 	}
 }
